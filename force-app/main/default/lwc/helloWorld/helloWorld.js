8936b0065b1a5ffa8e8367a075b2a69f04cc9909
VenkatLWC <javvaji66@gmail.com>
2020-06-16 07:26:41 +0530

    TuesdayChanges

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -3,6 +3,6 @@ import { LightningElement, track, api } from 'lwc';
 export default class HelloWorld extends LightningElement { 
     @api displayMessageApi= 'Hello Api Decorator';
     @track displayMessageTrack= 'Hello track Decorator';
-    displayMessage = 'world';
+    displayMessage = 'helloworld';
     @track objtest = {firstName:'VenkataRao',lastName:"Javvaji"}  ;    
 }
